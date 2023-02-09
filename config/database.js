d761ad5bfc9183a0e95ce17b42c3182ea9729afe
shivam <shivampal7207@gmail.com>
2023-02-09 13:13:49 +0530

    added slug and affiliates json

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
+      host: env('PGHOST', '34.168.237.66'),
+      port: env.int('PGPORT', 6787),
       database: env('PGDATABASE', 'strapi'),
       user: env('PGUSER', 'strapi'),
       password: env('PGPASSWORD', 'password'),
