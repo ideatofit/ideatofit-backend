89206a4c8da5b866d645c28179e547b88715145d
shivam <shivampal7207@gmail.com>
2023-03-14 19:01:06 +0530

    error resolved

---
+++ src/api/height-group/controllers/height-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * height-group controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::height-group.height-group');
