d7af861cca2869c4b1ec143d98df825492d8c0ea
shivam <shivampal7207@gmail.com>
2023-03-14 17:31:52 +0530

    added content in the recipes

---
+++ src/api/height-group/routes/height-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * height-group router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::height-group.height-group');
---
+++ src/api/height-group/services/height-group.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * height-group service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::height-group.height-group');
